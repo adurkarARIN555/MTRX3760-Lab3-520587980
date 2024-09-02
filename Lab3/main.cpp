5fdf12a20f1f86b4297e29f954748afe7636f4f8
adurkarARIN555 <adurkararin@gmail.com>
2024-09-02 18:40:22 +1000

    Initial code file
    Directories and files created

---
+++ Lab3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+#include "include/eLogicLevel.h"
+
+#include "include/LogicGate.h"
+
+#include "include/Wire.h"
+
+#include "include/AndGate.h"
+#include "include/OrGate.h"
+#include "include/NotGate.h"
+#include "include/XorGate.h"
+
+#include "include/ReadText.h"
+#include "include/BuildCircuit.h"
+#include "include/Test.h"
+
+int main(void)
+{
+    return 0;
+}
+
